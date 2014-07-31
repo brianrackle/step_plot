f24d7fe9719b4937f006cfc3a044883ab324b618
brianrackle <brianrackle@hotmail.com>
2014-07-31 14:09:38 -0700

    adding more markdown functions

--- BrainstemBreakfast/BrainstemBreakfast/Markdown.hpp
+++ BrainstemBreakfast/BrainstemBreakfast/Markdown.hpp
@@ -1,70 +1,87 @@
-//create markdown table header
-template<class S, class ... Types>
-inline void table_header(S && stream, const char * format, const char  * first_name, Types ... rest_name)
-{
-	stream << "| " << first_name << " ";
-	table_header(stream, format, rest_name...);
-}
+#include <ostream>
+#include <regex>
 
-//table_header recursion base case
-template <class S>
-inline void table_header(S && stream, const char * format)
+namespace bsb
 {
-	stream << "|\n";
-
-	for (char const* pos = format; *pos; ++pos)
-		switch (*pos)
+	//http://daringfireball.net/projects/markdown/basics
+	//objects: paragraph, table, blockqoute, inline qoute, list
+	//should convert to html equivalents
+	namespace markdown
 	{
-		case 'l':
-			stream << "|:--- ";
-			break;
-		case 'r':
-			stream << "| ---:";
-			break;
-		case 'c':
-			stream << "|:---:";
-			break;
-		default:
-			break;
+		//create markdown table header
+		template<class ... Types>
+		inline std::string table_header(const char * format, const char  * first_name, Types ... rest_name)
+		{
+			return std::string("| ") + first_name + " " + table_header(format, rest_name...);
+		}
+
+		//table_header recursion base case
+		inline std::string table_header(const char * format)
+		{
+			std::string output("|\n");
+
+			for (char const* pos = format; *pos; ++pos)
+				switch (*pos)
+			{
+				case 'l':
+					output += "|:--- ";
+					break;
+				case 'r':
+					output += "| ---:";
+					break;
+				case 'c':
+					output += "|:---:";
+					break;
+				default:
+					break;
+			}
+			return output + "|" + "\n";
+		}
+
+		//create markdown table row
+		template <typename ... Types>
+		inline std::string table_row(const char * first_content, Types  ... rest_content)
+		{
+			return std::string("| ") + first_content + " " + table_row(rest_content...);
+		}
+
+		//table_row recursion base case
+		inline std::string table_row()
+		{
+			return "|\n";
+		}
+
+		//create markdown heading
+		inline std::string heading(unsigned char const level, const char * name)
+		{
+			std::string output;
+			for (std::remove_const_t<decltype(level)> i = 0; i < level; ++i)
+				output += "#";
+			return output + " " + name + "\n";
+		}
+
+		//create an html anchor. To be paired with an anchor link
+		inline std::string anchor(const char * name)
+		{
+			return std::string("<a name = \"") + name + "\"></a>\n";
+		}
+
+		//create a markdown anchor link. To be paired with anchor
+		inline std::string anchor_link(const char * content, const char * name)
+		{
+			return std::string("[") + content + "](#" + name + ")\n";
+		}
+
+		//create a markdown paragraph
+		inline std::string pgf(const char * content)
+		{
+			return std::string("\n\n") + content + "\n\n";
+		}
+
+		inline std::string block_quote(const char * content)
+		{
+			std::regex regex("\n");
+			return "\n" + std::regex_replace(std::string("> ") + content, regex, "$&> ") + "\n";
+		}
 	}
-	stream << "|\n";
 }
-
-//create markdown table row
-template <class S, typename ... Types>
-inline void table_row(S && stream, const char * first_content, Types  ... rest_content)
-{
-	stream << "| " << first_content << " ";
-	table_row(stream, rest_content...);
-}
-
-//table_row recursion base case
-template <class S>
-inline void table_row(S && stream)
-{
-	stream << "|\n";
-}
-
-//create markdown heading
-template <class S>
-inline void heading(S && stream, unsigned char const level, const char * name)
-{
-	for (decltype(level) i = 0; i < level; ++i)
-		stream << "#";
-	stream << " " << name << "\n";
-}
-
-//create an html anchor. To be paired with an anchor link
-template <class S>
-inline void anchor(S && stream, const char * name)
-{
-	stream << "<a name = \"" << name << "\"></a>\n";
-}
-
-//create a markdown anchor link. To be paired with anchor
-template <class S>
-inline void anchor_link(S && stream, const char * content, const char * name)
-{
-	stream << "[" << content << "](#" << name << ")\n";
-}
-
