268c1d96c0d391493945e207504ea7565e5bd93e
brianrackle <brianrackle@hotmail.com>
2014-08-18 21:43:50 -0700

    removing unnecessary parens

--- BrainstemBreakfast/BrainstemBreakfast/range_map.hpp
+++ BrainstemBreakfast/BrainstemBreakfast/range_map.hpp
@@ -12,16 +12,13 @@ namespace range_map
 	class = typename std::enable_if<std::is_arithmetic<_TT>::value>::type>
 	_TT scale_value(_FT value, _FT lowestFrom, _FT highestFrom, _TT lowestTo, _TT highestTo)
 	{
-		if (value == lowestFrom) return lowestTo;		
-		if (value == highestFrom) return highestTo;
-
 		//scale by half to account for negative and positive range being too large to represent
 		const auto && fHLF = [](_FT v){ return v / 2; };
 		const auto && tHLF = [](_TT v){ return v / 2; };
 
 		auto scaledOffsetResult =
-			((tHLF(highestTo) - tHLF(lowestTo)) * 
-			((fHLF(value) - fHLF(lowestFrom)) / (fHLF(highestFrom) - fHLF(lowestFrom))));
+			(tHLF(highestTo) - tHLF(lowestTo)) * 
+			((fHLF(value) - fHLF(lowestFrom)) / (fHLF(highestFrom) - fHLF(lowestFrom)));
 
 		return (_TT)(scaledOffsetResult + lowestTo + scaledOffsetResult); //seperated to prevent overflow
 	}
