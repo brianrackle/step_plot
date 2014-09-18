54393f890171c5d79121509cd769f204e6f89fde
brianrackle <brianrackle@hotmail.com>
2014-09-18 05:04:29 +0000

    definition for outit

--- source/regex_replace_ext.hpp
+++ source/regex_replace_ext.hpp
@@ -13,18 +13,53 @@ namespace bsb
 {
 namespace regex_ext
 {
-
+  //honor match flag types
+  //see regex_token_iterator constructor:
+  //http://en.cppreference.com/w/cpp/regex/regex_token_iterator/regex_token_iterator
   //replace fmt with a function
-  /*
+ 
   template< class OutputIt, class BidirIt,
 	    class Traits, class CharT,
 	    class STraits, class SAlloc >
   OutputIt regex_replace( OutputIt out, BidirIt first, BidirIt last,
 			  const std::basic_regex<CharT,Traits>& re,
-			  const std::basic_string<CharT,STraits,SAlloc>& fmt,
-			  std::regex_constants::match_flag_type flags = 
-			  std::regex_constants::match_default );
+			  const typename std::common_type<std::function<std::basic_string<CharT,STraits,SAlloc> 
+			  (const unsigned, const std::basic_string<CharT,STraits,SAlloc> &)>>::type& fmt,
+			  std::regex_constants::match_flag_type flags =
+			  std::regex_constants::match_default )
+  {
+    std::vector<int> smatches{-1};
+    if(re.mark_count() == 0)
+      smatches.push_back(0);
+    else
+      {
+	smatches.resize(1+re.mark_count());
+	std::iota(std::next(smatches.begin()), smatches.end(), 1); //-1, 1, 2, etc...    
+      }
 
+    unsigned smatch_count = smatches.size();
+    unsigned count = 0;
+
+    std::regex_token_iterator
+      <typename std::basic_string<CharT,STraits,SAlloc>::const_iterator> 
+      tbegin(first, last, re, smatches), tend;            
+
+    std::for_each(tbegin, tend, [&count,&smatch_count,&out,&fmt]
+		  (const std::basic_string<CharT,STraits,SAlloc> & token)
+		  {
+		    if(token.size() != 0)
+		      {
+			if(!count) 
+			  out << token;
+			else
+			  out << fmt(count,token);
+		      }
+		    count = ++count % smatch_count;
+		  });
+    return out;
+  }
+
+  /*
   template< class OutputIt, class BidirIt,
 	    class Traits, class CharT >
   OutputIt regex_replace( OutputIt out, BidirIt first, BidirIt last,
@@ -33,7 +68,7 @@ namespace regex_ext
 			  std::regex_constants::match_flag_type flags = 
 			  std::regex_constants::match_default );
   */
-  //why does s and re work but not fmt
+
   //Matches regex and calls fmt for replacement value.
   //fmt is passed the sub-expression index and matched string and returns the replacement value
   template< class Traits, class CharT,
