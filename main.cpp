22e53a006000554c11b09c78d6a139e4eddc9de2
Brian Rackle <brianrackle@hotmail.com>
2014-10-15 17:53:51 -0700

    initializing repo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//need to include /usr/include/ and /usr/lib/ to compile
+//http://www.gnu.org/software/ncurses/
+#include "ncurses.h"
+#include <string>
+#include <regex>
+
+
+//http://kunak.phsx.ku.edu/~sergei/Gnuplot/line_point_types.html
+int main()
+{
+  //create "steps/" directory
+
+  //load data.txt
+  //parse into individual steps.dat files
+
+  //copy appropriate steps_#.dat file to local directory as current.dat
+  //set datafile commentschars "#"
+}
