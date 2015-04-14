4cb32c813501d7db00ebdda51031a4c78cf599fe
Brian Rackle <brianrackle@hotmail.com>
2015-04-13 22:43:42 -0700

    added coordinate systems for intuitive data access

--- step_log/step_log.h
+++ step_log/step_log.h
@@ -4,69 +4,88 @@
 #include <vector>
 #include <cereal/archives/portable_binary.hpp>
 
+namespace x_dimensional {
+    using d_t = std::size_t;
+
+    namespace euclidian {
+        constexpr d_t x = 0, y = 1, z = 2;
+    }
+
+    namespace homogenous {
+        constexpr d_t x = 0, y = 1, z = 2, w = 3;
+    }
+
+    namespace rgba {
+        constexpr d_t r = 0, g = 1, b = 2, a = 3;
+    }
+
+    namespace cymk {
+        constexpr d_t c = 0, y = 1, m = 2, k = 3;
+    }
+
+    template<d_t Dimension, class T>
+    class xd;
+
+    template<class T>
+    class xd<1, T> {
+    public:
+        xd() { }
+
+        xd(const T &v) : value(v) { }
+
+        T value = {};
+    };
+
+    template<d_t Dimension, class T>
+    class xd {
+    public:
+        xd() { }
+
+        template<class ... Ts>
+        xd(const T &v, const Ts &... vs) : value(v), tail(vs...) { }
+
+        T value = {};
+        xd<Dimension - 1, T> tail;
+    };
+
+    template<class T, class ... Ts>
+    auto make_point_xd(const T &v, const Ts &... vs) {
+        return xd<1 + sizeof...(vs), T>(v, vs...);
+    }
+
+    template<d_t D, d_t Dimension, class T>
+    class get_helper;
+
+    template<d_t Dimension, class T>
+    class get_helper<0, Dimension, T> {
+    public:
+        auto &operator()(const xd<Dimension, T> &v) {
+            return v.value;
+        }
+    };
+
+    template<d_t D, d_t Dimension, class T>
+    class get_helper {
+    public:
+        auto &operator()(const xd<Dimension, T> &v) {
+            get_helper<D - 1, Dimension - 1, T> helper;
+            return helper(v.tail);
+        }
+    };
+
+    template<d_t D, d_t Dimension, class T>
+    auto &get(const xd<Dimension, T> &v) {
+        get_helper<D, Dimension, T> helper;
+        return helper(v);
+    }
+
+    template<d_t Dimension, class T>
+    class primitive {
+        std::vector<xd<Dimension, T>> data;
+    };
+}
+
 //TODO(brian): finish plot definitions
-using d_t = std::size_t;
-
-template<d_t Dimension, class T>
-class point_xd;
-
-template<class T>
-class point_xd<1, T> {
-public:
-    point_xd() { }
-
-    point_xd(const T &v) : value(v) { }
-
-    T value = {};
-};
-
-template<d_t Dimension, class T>
-class point_xd {
-public:
-    point_xd() { }
-
-    template<class ... Ts>
-    point_xd(const T &v, const Ts &... vs) : value(v), tail(vs...) { }
-
-    T value = {};
-    point_xd<Dimension - 1, T> tail;
-};
-
-template<class T, class ... Ts>
-auto make_point_xd(const T &v, const Ts &... vs) {
-    return point_xd<1 + sizeof...(vs), T>(v, vs...);
-}
-
-template<d_t D, d_t Dimension, class T>
-class get_helper;
-
-template<d_t Dimension, class T>
-class get_helper<0, Dimension, T> {
-public:
-    auto &operator()(const point_xd<Dimension, T> &v) {
-        return v.value;
-    }
-};
-
-template<d_t D, d_t Dimension, class T>
-class get_helper {
-public:
-    auto &operator()(const point_xd<Dimension, T> &v) {
-        get_helper<D - 1, Dimension - 1, T> helper;
-        return helper(v.tail);
-    }
-};
-
-template<d_t D, d_t Dimension, class T>
-auto &get(const point_xd<Dimension, T> &v) {
-    get_helper<D, Dimension, T> helper;
-    return helper(v);
-}
-
-template<d_t Dimension, class T>
-class primitive {
-    std::vector<point_xd<Dimension, T>> data;
-};
 
 //template <class T>
 //class points : public primitive
--- step_log/step_log_test.cpp
+++ step_log/step_log_test.cpp
@@ -1,14 +1,24 @@
 #include "step_log.h"
 
 int main(int argc, char **argv) {
-    //TODO(brian): put in namespace cartesian
-    const d_t x = 0, y = 1, z = 2;
+    using namespace x_dimensional;
 
-    point_xd<3, double> point0(1, 2, 3); //type point_xd<3, double>
-    auto point1 = make_point_xd<double>(1, 2, 3); //type point_xd<3,double>
-    auto point2 = make_point_xd(1, 2, 3); //type point_xd<3,int>
-    point_xd<3, double> point3;
-    auto value0 = get<x>(point0);
-    auto value1 = get<y>(point0);
-    auto value2 = get<z>(point0);
+    xd<3, double> point0(1, 2, 3); //type xd<3, double>
+    auto point1 = make_point_xd<double>(1, 2, 3); //type xd<3,double>
+    auto point2 = make_point_xd(1, 2, 3); //type xd<3,int>
+    xd<3, double> point3; //values are default constructed
+    {
+        using namespace euclidian;
+        auto value0 = get<x>(point0); //value0 == 1
+        auto value1 = get<y>(point0); //value1 == 2
+        auto value2 = get<z>(point0); //value2 == 3
+    }
+    {
+        using namespace rgba;
+        auto value0 = get<r>(point0);
+        auto value1 = get<g>(point1);
+        auto value2 = get<b>(point2);
+        //TODO(brian): assert get<N>(xd(K)) N <= K
+    }
+
 }
